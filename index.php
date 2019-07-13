5da0a4c1c1c0071f998e02ed2069abf902e38d82
xakepok@xakepok.com <44duP_yx579>
2019-07-13 17:18:44 +0300

    Mobile font-size

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ TplRwtmplHelper::setMetadata();
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
-	<jdoc:include type="head" />
-    <link rel="icon" href="/templates/<?php echo $this->template;?>/images/ico_64.png" type="image/png">
+    <jdoc:include type="head"/>
+    <link rel="icon" href="/templates/<?php echo $this->template; ?>/images/ico_64.png" type="image/png">
 </head>
 <body class="<?php echo TplRwtmplHelper::setBodySuffix(); ?>">
 <?php //echo tplRwtmplHelper::setAnalytics(0, 'your-analytics-id'); ?>
@@ -22,12 +22,12 @@ TplRwtmplHelper::setMetadata();
         <div class="container-fluid text-center">
             <div class="row">
                 <div class="col">
-                    <img class="img-fluid" src="/images/logo_site.png" alt="Logo" />
+                    <a href="/"><img class="img-fluid" src="/images/logo_site.png" alt="Logo"/></a>
                 </div>
             </div>
             <div class="row">
                 <div id="nav" class="col">
-                    <jdoc:include type="modules" name="menu" style="none" />
+                    <jdoc:include type="modules" name="menu" style="none"/>
                 </div>
             </div>
         </div>
@@ -36,18 +36,24 @@ TplRwtmplHelper::setMetadata();
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-2">
-                    <?php if ($this->countModules('left')) : ?>
-                        <jdoc:include type="modules" name="left" style="xhtml" />
-                    <?php endif; ?>
+                    <div class="container-fluid">
+                        <?php if ($this->countModules('left')) : ?>
+                            <jdoc:include type="modules" name="left" style="xhtml"/>
+                        <?php endif; ?>
+                    </div>
                 </div>
                 <div class="col-lg-8">
-                    <jdoc:include type="message" />
-                    <jdoc:include type="component" />
+                    <div class="container-fluid" id="component">
+                        <jdoc:include type="message"/>
+                        <jdoc:include type="component"/>
+                    </div>
                 </div>
                 <div class="col-lg-2">
-                    <?php if ($this->countModules('right')) : ?>
-                        <jdoc:include type="modules" name="right" style="xhtml" />
-                    <?php endif; ?>
+                    <div class="container-fluid">
+                        <?php if ($this->countModules('right')) : ?>
+                            <jdoc:include type="modules" name="right" style="xhtml"/>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -56,14 +62,16 @@ TplRwtmplHelper::setMetadata();
         <div class="container-fluid">
             <div class="row">
                 <div class="col">
-                    <?php if ($this->countModules('footer')) : ?>
-                        <jdoc:include type="modules" name="footer" style="xhtml" />
-                    <?php endif; ?>
+                    <div class="container-fluid">
+                        <?php if ($this->countModules('footer')) : ?>
+                            <jdoc:include type="modules" name="footer" style="xhtml"/>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col">
-                    <jdoc:include type="modules" name="debug" style="block" />
+                    <jdoc:include type="modules" name="debug" style="block"/>
                 </div>
             </div>
         </div>
